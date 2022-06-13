45dcf20b15928dc27e66b900dd5d6fca4e294cff
jyh926 <jiayanhong@huawei.com>
2022-06-13 15:46:44 +0800

    ril_adapter芯片组件解耦，去除无效依赖
    Signed-off-by: jyh926 <jiayanhong@huawei.com>

---
+++ utils/native/include/telephony_log_c.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TELEPHONY_LOG_C_H
+#define TELEPHONY_LOG_C_H
+
+#include <stddef.h>
+
+#include "hilog/log.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef LOG_DOMAIN
+#define LOG_DOMAIN 0xD002B01
+#endif
+#ifndef LOG_TAG
+#define LOG_TAG "TelephonySubsystem"
+#endif
+
+#define OHOS_DEBUG
+#ifndef OHOS_DEBUG
+#define DECORATOR_HILOG(op, fmt, args...) \
+    do {                                  \
+        op(LOG_CORE, fmt, ##args);        \
+    } while (0)
+#else
+// Gets the raw file name of the file.
+// Its advantage is that it is executed only once __builtin_strrchr() function
+inline const char *GetRawFileName(const char *path)
+{
+    const char *last = __builtin_strrchr(path, '/');
+    return (last != NULL) ? (last + 1) : path;
+}
+
+#define DECORATOR_HILOG(op, fmt, args...)                                                                           \
+    do {                                                                                                            \
+        op(LOG_CORE, "[%{public}s-(%{public}s:%{public}d)] " fmt, __FUNCTION__, GetRawFileName(__FILE__), __LINE__, \
+            ##args);                                                                                                \
+    } while (0)
+#endif
+
+#define TELEPHONY_LOGE(fmt, args...) DECORATOR_HILOG(HILOG_ERROR, fmt, ##args)
+#define TELEPHONY_LOGW(fmt, args...) DECORATOR_HILOG(HILOG_WARN, fmt, ##args)
+#define TELEPHONY_LOGI(fmt, args...) DECORATOR_HILOG(HILOG_INFO, fmt, ##args)
+#define TELEPHONY_LOGF(fmt, args...) DECORATOR_HILOG(HILOG_FATAL, fmt, ##args)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TELEPHONY_LOG_C_H
---
+++ utils/native/include/telephony_log_wrapper.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TELEPHONY_LOG_WRAPPER_H
+#define TELEPHONY_LOG_WRAPPER_H
+
+#include <string>
+
+#include "hilog/log.h"
+
+namespace OHOS {
+namespace Telephony {
+#ifndef LOG_DOMAIN
+#define LOG_DOMAIN 0xD002B01
+#endif
+#ifndef TELEPHONY_LOG_TAG
+#define TELEPHONY_LOG_TAG "TelephonySubsystem"
+#endif
+
+static constexpr OHOS::HiviewDFX::HiLogLabel TELEPHONY_LABEL = {LOG_CORE, LOG_DOMAIN, TELEPHONY_LOG_TAG};
+
+#define OHOS_DEBUG
+#ifndef OHOS_DEBUG
+#define PRINT_TELEPHONY_LOG(op, fmt, ...) (void)OHOS::HiviewDFX::HiLog::op(TELEPHONY_LABEL, fmt, ##__VA_ARGS__)
+#else
+// Gets the raw file name of the file.
+// This function is a function executed by the compiler, that is,
+// it has been executed at compile time. When the program runs,
+// it directly refers to the value calculated by this function
+// and does not consume CPU for calculation.
+inline constexpr const char *GetRawFileName(const char *path)
+{
+    char ch = '/';
+    const char *start = path;
+    // get the end of the string
+    while (*start++) {
+        ;
+    }
+    while (--start != path && *start != ch) {
+        ;
+    }
+
+    return (*start == ch) ? ++start : path;
+}
+
+#define PRINT_TELEPHONY_LOG(op, fmt, ...)                                                                        \
+    (void)OHOS::HiviewDFX::HiLog::op(TELEPHONY_LABEL, "[%{public}s-(%{public}s:%{public}d)] " fmt, __FUNCTION__, \
+        GetRawFileName(__FILE__), __LINE__, ##__VA_ARGS__)
+#endif
+
+#define TELEPHONY_LOGE(fmt, ...) PRINT_TELEPHONY_LOG(Error, fmt, ##__VA_ARGS__)
+#define TELEPHONY_LOGW(fmt, ...) PRINT_TELEPHONY_LOG(Warn, fmt, ##__VA_ARGS__)
+#define TELEPHONY_LOGI(fmt, ...) PRINT_TELEPHONY_LOG(Info, fmt, ##__VA_ARGS__)
+#define TELEPHONY_LOGF(fmt, ...) PRINT_TELEPHONY_LOG(Fatal, fmt, ##__VA_ARGS__)
+} // namespace Telephony
+} // namespace OHOS
+#endif // OHOS_TELEPHONY_LOG_WRAPPER_H
